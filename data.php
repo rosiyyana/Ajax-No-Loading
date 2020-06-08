55a5509ef813d653a2236ba5705a81498fa894b9
rosiyyana <63337889+rosiyyana@users.noreply.github.com>
2020-06-08 13:31:56 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
 <table id="example" class="table table-striped table-bordered" style="width:100%">
     <thead>
         <tr>
@@ -25,9 +15,9 @@
             include 'koneksii.php';
             $no = 1;
             $query = "SELECT * FROM tbl_siswa ORDER BY id DESC";
-            $prepare1 = $db1->prepare($query);
-            $prepare1->execute();
-            $res1 = $prepare1->get_result();
+            $dewan1 = $db1->prepare($query);
+            $dewan1->execute();
+            $res1 = $dewan1->get_result();
  
             if ($res1->num_rows > 0) {
                 while ($row = $res1->fetch_assoc()) {
@@ -60,58 +50,58 @@
  
  
 <script type="text/javascript">
+
     $(document).ready(function() {
         $('#example').DataTable();
     } );
 
-    function reset() {
-        document.getElementById("err_nama_siswa").innerHTML = "" ;
-        document.getElementById("err_alamat").innerHTML = "" ;
-        document.getElementById("err_jurusan").innerHTML = "" ;
-        document.getElementById("err_tanggal_masuk").innerHTML = "" ;
-        document.getElementById("err_jenkel").innerHTML = "" ;
 
-    }
-
-    $(document).on('click', '.edit_data', function () {
-        var id = $(this).attr('id');
-        $.ajax({
-            type: 'POST',
-            url: "get_data_all.php",
-            data: {id:id},
-            dataType: 'json',
-            success: function (response) {
-                reset();
-                document.getElementByI("id").value = response.id;
-                document.getElementByI("nama_siswa").value = response.nama_siswa;
-                document.getElementByI("tanggal_masuk").value = response.tanggal_masuk;
-                document.getElementByI("alamat").value = response.alamat;
-                document.getElementByI("jurusan").value = response.jurusan;
-                    if (response.jenis_kelamin=="Laki-laki") {
-                        document.getElementById)("jenkel1").checked = true;
-                    }
-                    else{
-                        document.getElementById)("jenkel2").checked = true;
-                    }
-            }, error: function (response) { 
-                console.log(response.reponseText);
-             }
-        });
+function reset(){
+    document.getElementById("err_nama_siswa").innerHTML = "";
+    document.getElementById("err_alamat").innerHTML = "";
+    document.getElementById("err_jurusan").innerHTML = "";
+    document.getElementById("err_tanggal_masuk").innerHTML = "";
+    document.getElementById("err_jenkel").innerHTML = "";
+}
+ 
+$(document).on('click', '.edit_data', function(){
+    var id = $(this).attr('id');
+    $.ajax({
+        type: 'POST',
+        url: "get_data.php",
+        data: {id:id},
+        dataType:'json',
+        success: function(response) {
+            reset();
+            $('html, body').animate({ scrollTop: 30 }, 'slow');
+            document.getElementById("id").value = response.id;
+            document.getElementById("nama_siswa").value = response.nama_siswa;
+            document.getElementById("tanggal_masuk").value = response.tgl_masuk;
+            document.getElementById("alamat").value = response.alamat;
+            document.getElementById("jurusan").value = response.jurusan;
+            if (response.jenis_kelamin=="Laki-laki") {
+                document.getElementById("jenkel1").checked = true;
+            } else {
+                document.getElementById("jenkel2").checked = true;
+            }
+        }, error: function(response){
+           console.log(response.responseText);
+        }
     });
-    $(document).on('click', '.hapus_data' function () {
-        var id = $(this).attr('id');
-        $.$.ajax({
-            type: 'POST',
-            url: "hapus_data.php",
-            data: {id:id},
-            success: function () {
-                $('.data').load("data.php");
-            }, error: function (response) { 
-                console.log(response.reponseText);
-             }
-        });
-      });
+});
+
+$(document).on('click', '.hapus_data', function(){
+    var id = $(this).attr('id');
+    $.ajax({
+        type: 'POST',
+        url: "hapus_data.php",
+        data: {id:id},
+        success: function() {
+            $('.data').load("data.php");
+        }, error: function(response){
+            console.log(response.responseText);
+        }
+    });
+});
+
 </script>
-    
-</body>
-</html>
